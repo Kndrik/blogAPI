cde541bc7606b9fe08fc6ee64062e2eb96b80989
Charles Lamarque <charles.lamarque69@gmail.com>
2023-07-06 12:52:43 +0200

    Add dashboard sidebar

--- admin-client/src/components/Sidebar.js
+++ admin-client/src/components/Sidebar.js
@@ -1,5 +1,47 @@
+import { useNavigate } from "react-router-dom";
+import logout from "../svgs/logout.svg";
+import plus from "../svgs/plus.svg";
+
 const Sidebar = (props) => {
-  return <div>Sidebar</div>;
+  const navigate = useNavigate();
+
+  const handleLogoClick = () => {
+    navigate("/dashboard");
+  };
+
+  const handleLogOutClick = () => {
+    localStorage.removeItem("blogJWT");
+    navigate("/login");
+  };
+
+  const handleNewArticleClick = () => {
+    navigate("/dashboard/new");
+  };
+
+  return (
+    <div className="w-20 py-5 flex flex-col justify-between items-center shadow-2xl px-4">
+      <div className="flex flex-col items-center w-full">
+        <button
+          onClick={handleLogoClick}
+          className="mb-5 mt-5 w-full hover:bg-blue-200 aspect-square bg-blue-100 shadow-md rounded-lg font-extrabold text-blue-600 text-md hover:cursor-pointer"
+        >
+          Blog
+        </button>
+        <button
+          onClick={handleNewArticleClick}
+          className="mb-5 aspect-square bg-blue-500 hover:bg-blue-600 flex justify-center items-center w-full p-1 rounded-lg"
+        >
+          <img src={plus} alt="plus logo" className="hover:cursor-pointer" />
+        </button>
+      </div>
+      <button
+        onClick={handleLogOutClick}
+        className=" bg-red-500 hover:bg-red-600 flex justify-center items-center w-full aspect-square p-1 rounded-lg"
+      >
+        <img src={logout} alt="logout logo" className="hover:cursor-pointer" />
+      </button>
+    </div>
+  );
 };
 
 export default Sidebar;
--- admin-client/src/pages/Dashboard.js
+++ admin-client/src/pages/Dashboard.js
@@ -1,24 +1,17 @@
 import useAuthRedirect from "../hooks/useAuthRedirect";
 import { useNavigate } from "react-router-dom";
 
+import Sidebar from "../components/Sidebar";
+
 const Dashboard = (props) => {
   useAuthRedirect();
-  const navigate = useNavigate();
-
-  const handleLogOut = () => {
-    localStorage.removeItem("blogJWT");
-    navigate("/login");
-  };
 
   return (
-    <div className="dashboard">
-      <div className="text-xl text-center font-extrabold mt-3">Dashboard</div>
-      <button
-        onClick={handleLogOut}
-        className="rounded-lg bg-blue-600 text-white font-bold text-sm px-4 py-2 hover:bg-blue-700"
-      >
-        Log out
-      </button>
+    <div className="flex min-h-screen">
+      <Sidebar />
+      <div className="flex-1 p-5 bg-gray-100 min-h-full">
+        <div className="text-xl text-center font-extrabold mt-3">Dashboard</div>
+      </div>
     </div>
   );
 };
