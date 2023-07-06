5a6834f2356309e75852ffe1ed9df4b57290e7be
Charles Lamarque <charles.lamarque69@gmail.com>
2023-07-06 18:54:37 +0200

    Remove unused page component

--- admin-client/src/pages/Redirect.js
+++
@@ -1,9 +0,0 @@
-import useAuthRedirect from "../hooks/useAuthRedirect";
-
-const Redirect = (props) => {
-  useAuthRedirect();
-
-  return <div className="Redirect">Redirect page</div>;
-};
-
-export default Redirect;
