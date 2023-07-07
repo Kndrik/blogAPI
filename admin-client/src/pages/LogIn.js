ea76c85fa3911618dfc64b902610bbcd671ee16b
Charles Lamarque <charles.lamarque69@gmail.com>
2023-07-07 12:27:59 +0200

    Fix typo

--- admin-client/src/pages/LogIn.js
+++ admin-client/src/pages/LogIn.js
@@ -65,7 +65,7 @@ const Login = (props) => {
           ></input>
         </div>
         <button
-          type="subnit"
+          type="submit"
           className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-md w-full sm:w-auto px-5 py-2.5 text-center"
         >
           Log In
